635c26ea76d772de9eeec56068fd5993287ee6ce
alireza darbandi <darbandi1996@gmail.com>
2020-11-26 16:33:47 +0330

    implemented some logs

--- server/src/api/handlers/collections.js
+++ server/src/api/handlers/collections.js
@@ -1,11 +1,23 @@
-
-const handler = module.exports = {};
+const handler = (module.exports = {});
 
 /**
  * get collection records
  * @param {object} options
  * @param {object} options.message
  */
-handler.collectionsGet = async ({message}) => {
+const collections = [];
+handler.collectionsGet = async ({ message }) => {
   // Implement your business logic here...
+  const { payload } = message;
+  console.log(message.headers);
+  let id = 1;
+  for (let i = 0; i < collections.length; i++) {
+    if (collections[i].id === id) id++;
+  }
+  collections.push({
+    id,
+    createdAt: new Date(),
+    data: payload,
+  });
+  console.log(collections);
 };
